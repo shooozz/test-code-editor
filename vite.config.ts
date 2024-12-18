dab62c2b1231e490af4ef1cc48aea7e542ceb11b
shooozz <vodolb@mail.ru>
2024-12-19 01:43:28 +0300

    Create project

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import EnvironmentPlugin from "vite-plugin-environment";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), EnvironmentPlugin("all", { prefix: "VITE_" })],
-  resolve: {
-    alias: [
-      {
-        find: "pages",
-        replacement: "/src/pages",
-      },
-    ],
-  },
+  plugins: [react()],
 });
